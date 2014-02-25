4da46c81765157ecd1fdb5bedf92fd31a2b24a7b
AlexandrosRi <arizos@dmst.aueb.gr>
2014-02-25 02:55:05 +0200

    Signed-off-by: AlexandrosRi <arizos@dmst.aueb.gr>

--- src/com/dmstannouncements/Main.java
+++ src/com/dmstannouncements/Main.java
@@ -12,24 +12,24 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class Main extends Activity  {
-	
+
 	private final String TAG = "main";
-	
+
 	//private static final int SETTINGS_RESULT = 1;
 	public static final String PREFS_NAME = "custom_rss";
-		  
+
 	private static final String URL1 = "http://my.dmst.aueb.gr/rssfeed.php";
 	private static final String URL2 = "http://www.dmst.aueb.gr/index.php/el/eventslist?format=feed&type=rss";
 	private static final String URL3 = "http://www.dmst.aueb.gr/index.php/el/newslist?format=feed&type=rss";
-	
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
     	SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-    	
+
         final Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -63,7 +63,7 @@ public class Main extends Activity  {
 
         final Button button4 = (Button) findViewById(R.id.button4);
         button4.setText(sharedPrefs.getString("rssName1", "Custom RSS"));
-        
+
         final String URL4 = sharedPrefs.getString("rssLink1", "http://my.dmst.aueb.gr/rssfeed.php");
         button4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -75,7 +75,7 @@ public class Main extends Activity  {
 
         final Button button5 = (Button) findViewById(R.id.button5);
         button5.setText(sharedPrefs.getString("rssName2", "Custom RSS"));
-        
+
         final String URL5 = sharedPrefs.getString("rssLink2", "http://my.dmst.aueb.gr/rssfeed.php");
         button5.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -87,7 +87,7 @@ public class Main extends Activity  {
 
         final Button button6 = (Button) findViewById(R.id.button6);
         button6.setText(sharedPrefs.getString("rssName3", "Custom RSS"));
-        
+
         final String URL6 = sharedPrefs.getString("rssLink3", "http://my.dmst.aueb.gr/rssfeed.php");
         button6.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -107,5 +107,5 @@ public class Main extends Activity  {
         });
 
 
-    }   
+    }
 }
